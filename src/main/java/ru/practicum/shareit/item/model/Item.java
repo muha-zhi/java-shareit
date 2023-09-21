c5d55b30bdc7c464f9c1bec564c838b31e5ead1e
marseille <zhiletezhevmuhamed-ali@yandex.ru>
2023-09-22 00:55:10 +0300

    Исправил  ошибки стиля

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
