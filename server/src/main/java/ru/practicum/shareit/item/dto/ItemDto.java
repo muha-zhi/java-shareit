51328e154841daec97436c15897f4ea3e418ba19
marseille <zhiletezhevmuhamed-ali@yandex.ru>
2023-11-04 19:05:44 +0300

    добавил модуль валидации запросов

---
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,29 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto {
+
+    private Long id;
+
+    @NotNull(message = "имя вещи не может быть пустым")
+    @NotBlank(message = "имя вещи не может быть пустым")
+    private String name;
+
+    @NotNull(message = "описание вещи не может быть пустым")
+    @NotBlank(message = "описание вещи не может быть пустым")
+    private String description;
+
+    @NotNull(message = "поле доступа вещи не может быть пустым")
+    private Boolean available;
+
+    private Long requestId;
+}
